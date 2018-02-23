edf6573b85159932248c3b957b4baec06f1e5fb8
Daniel Wong <drojfjord@gmail.com>
2018-02-23 13:53:21 +1100

    Patches required for see #43 - Update patch utility to change the save directory from 'saves' to 'mysav'. Update 0.utf to check for 'big_umineko_op5' and op7 and prefer them to the original files

--- exe_patch_utils/patchUminekoAllLoad0xutf.py
+++ exe_patch_utils/patchUminekoAllLoad0xutf.py
@@ -15,23 +15,28 @@ def get_input_files() -> [str]:
 print('Patching the following files:', get_input_files())
 
 for file_to_patch in get_input_files():
+    print('\n\nBegin Patching', file_to_patch)
     with open(file_to_patch, "rb") as f:
         exe_byte_array = f.read()
 
         #these will have a null character (\x00) appended to them before the replacement is performed
         string_replacements_required = [
-        (b'0.utf', b'0.u\x00\x00'),  #this forces game to check for '0.ut', and only run if that file exists
-        (b'%d.%s', b'%dx%s'),     #this prevents 0.utf being loaded
-        (b'%02d.%s', b'%d.%.1s'), #this loads the files 0.ut, 1.ut, 2.ut ...
+        (b'0.utf', b'0.u\x00\x00', 1),  #this forces game to check for '0.ut', and only run if that file exists
+        (b'%d.%s', b'%dx%s', 1),        #this prevents 0.utf being loaded
+        (b'%02d.%s', b'%d.%.1s', 1),    #this loads the files 0.ut, 1.ut, 2.ut ...
+        (b'saves/', b'mysav/', 2),      #this changes the save folder location
         ]
 
-        for before_string, after_string in string_replacements_required:
-            (exe_byte_array, num_replacements) = re.subn(before_string + b'\x00', after_string + b'\x00', exe_byte_array, count=1)
-            print('Replaced String {} to {} {} times'.format(before_string, after_string, num_replacements))
-            if num_replacements != 1:
-                print("Error: couldn't patch {} string to {}", before_string, after_string)
-                exit(0)
+        for before_string, after_string, max_replacements in string_replacements_required:
+            #check number of time of the search string occurs in the file, as a check
+            num_ocurrances = len(re.findall(before_string + b'\x00', exe_byte_array))
 
+            (exe_byte_array, num_replacements) = re.subn(before_string + b'\x00', after_string + b'\x00', exe_byte_array, count=max_replacements)
+            print('Replaced String {} to {} {} times (occurs {} times in file)'.format(before_string, after_string, num_replacements, num_ocurrances))
+            if num_replacements == 0:
+                print("Error: couldn't patch {} string to {}".format(before_string, after_string))
+                exit(0)
+                
 
     #save the file to disk only if all the above patches were successful
     with open('run_u_ext_' + file_to_patch, 'wb') as outFile:
@@ -45,3 +50,6 @@ print('The patched .exes will now launch "0.u" instead of "0.utf".')
 #Please see the function "int ScriptHandler::readScript(DirPaths *path, const char* prefer_name)" in ScriptHandler.cpp of Ponscripter
 # the extra changes are necessary due to how the game scans for script files - it will always (mostly always) scan for 0.utf, 1.utf 2 ... and also 00.utf, 01.utf etc
 # The filename must be included in the scan range, hence the modification to search for 0.ut, 1.ut ... instead of 00.utf, 01.utf ....
+#
+# On linux only, I noticed the '0.utf' string and the '00.utf' string are compressed together so you only see '00.utf' in the binary.
+# I think the patch will still work anyway.
