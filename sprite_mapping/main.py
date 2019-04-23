f4969a10e05367564135b0950c6d96d6e80c7346
Daniel Wong <drojfjord@gmail.com>
2019-04-23 22:51:41 +1000

    add sprite mode lookup class

--- sprite_mapping/main.py
+++ sprite_mapping/main.py
@@ -1,4 +1,24 @@
 import re
+from typing import Dict
+
+
+class SpriteModeLookup:
+    def __init__(self):
+        self.map = {} #type: Dict[str, int]
+
+    def set_sprite_type(self, name: str, value: int):
+        self.map[name] = int(value)
+
+    def get_sprite_type(self, name: str) -> int:
+        """
+        The default type for a sprite is '1'
+        :param name: sprite name, eg KUM
+        :return: the number of the sprite
+        """
+        return self.map.get(name, 1)
+
+    def reset_sprite_types(self):
+        self.map.clear()
 
 
 def getAddOrRemovedLineGroupsFromDiff(diffAllLines):
