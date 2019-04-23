04ca49217bb27a220b2e0c117a34476dce3ec874
Daniel Wong <drojfjord@gmail.com>
2019-04-23 22:51:41 +1000

    Add sprite mode lookup class

--- sprite_mapping/main.py
+++ sprite_mapping/main.py
@@ -1,47 +1,17 @@
 import re
+from lib import *
 
+change_clothes_simple = re.compile(r'^mov([^_]+)_Isyou')
+change_clothes_command = re.compile(r'^mov\s+%([^_]+)_Isyou\s*,\s*(\d+)', re.IGNORECASE)
 
-def getAddOrRemovedLineGroupsFromDiff(diffAllLines):
-    """
-    Returns a List[List[str]]
-    :param diffAllLines: A List[str] - all the lines in the diff
-    :return: Each inner list of contains some +lines (the old script ld) and some -lines (the new script ld)
-    The returned list still has the '+' and '-' at the start of the line so you can differentiate which lines were added/removed
-    """
-    section_list = []
-    current_section = None
-    section_started = False
+chapter_start = re.compile(r'^\*umi[5678]', re.IGNORECASE) #start of chapter or subchapter
+teatime_start = re.compile(r'^\*teatime', re.IGNORECASE)
+question_mark_teatime_start = re.compile(r'^\*ura', re.IGNORECASE)
+eye_command = re.compile(r'^eye', re.IGNORECASE)
+reset_regexes = [eye_command, chapter_start, teatime_start, question_mark_teatime_start]
 
-    for line in diffAllLines:
-        if line[0] == '@':
-            continue
-
-        if line[0] not in [' ', '+', '-']:
-            raise Exception("invalid line start character: ", line[0])
-
-        if section_started:
-            # a ' ' ends a section
-            if line[0] == ' ':
-                section_list.append(current_section)
-                current_section = None
-                section_started = False
-            else:
-                current_section.append(line)
-        else:
-            # a + or - starts a section
-            if line[0] != ' ':
-                section_started = True
-                current_section = [line]
-
-    if current_section is not None and len(current_section) > 0:
-        section_list.append(current_section)
-
-def getLineAndReplacementLines(diffLineGroups):
-    """
-
-    :param diffLineGroups: a list of line groups from getAddOrRemovedLineGroupsFromDiff
-    :return:
-    """
+character_alias = re.compile(r'(\w{3})_(\w+)[^\w.]', re.IGNORECASE)
+PS3SpriteLineToSpritePathRegex = re.compile(r'(sprites\\[^"]+)', re.IGNORECASE)
 
 with open('sprite_diff.txt', 'r', encoding='utf-8') as sprite_diff_file:
     diffAllLines = sprite_diff_file.readlines()
@@ -49,110 +19,66 @@ with open('sprite_diff.txt', 'r', encoding='utf-8') as sprite_diff_file:
 with open(r'C:\drojf\large_projects\umineko\umineko-answer\0.utf', 'r', encoding='utf-8') as sprite_diff_file:
     currentScriptAllLines = sprite_diff_file.readlines()
 
-change_clothes_simple = re.compile(r'^mov([^_]+)_Isyou')
-change_clothes_command = re.compile(r'^mov\s+%([^_]+)_Isyou\s*,\s*(\d+)', re.IGNORECASE)
+sprite_clothes_lookup = SpriteModeLookup()
 
+chunks = [[]]
 
-chapter_start = re.compile(r'^\*umi[5678]', re.IGNORECASE) #start of chapter or subchapter
-teatime_start = re.compile(r'^\*teatime', re.IGNORECASE)
-question_mark_teatime_start = re.compile(r'^\*ura', re.IGNORECASE)
-eye_command = re.compile(r'^eye', re.IGNORECASE)
-
-reset_regexes = [eye_command, chapter_start, teatime_start, question_mark_teatime_start]
-
-
-# NOTES:
-# Calling *DATA_SET resets sprites. Doesn't need to be checked as happens when you first start the game.#
-# (*jump_sysdata_set1_1/2/3) should resets sprites (calls DATA_SET), but doesn't need to be checked because it is called each time you start a chapter (see below)
-
-# - Chapter jump markers also seem to reset sprites, but might be unnecessary
-# - Teatime should implicitly clear the sprites the because you restart the game?
-# - Start of ??? should implicitly clear sprites because you restart the game
-# - Calling the *eye functions reset sprites (calls DATA_SET)
-
-# If the wrong sprite is classified, should be easy to tell unless it is 'never used'.
-# As all? the outfits are used, should be pretty easy to identify these cases.
-
-for line in diffAllLines:
-    fullMatch = change_clothes_command.search(line[1:])
-    if fullMatch:
-        print(fullMatch.group(1), fullMatch.group(2), line)
-
-    #check for irregular clothes change lines
-    simpleMatch = change_clothes_simple.search(line[1:])
-    if simpleMatch and not fullMatch:
+# The first five lines are assumed to have the diff header, so skip them.
+for line in diffAllLines[5:]:
+    # search for a clothes change command
+    m = change_clothes_command.search(line[1:])
+    if m:
+        character_name = m.group(1)
+        clothes_number = int(m.group(2))
+        # print(character_name, clothes_number, line)
+        sprite_clothes_lookup.set_sprite_type(character_name, clothes_number)
+    elif change_clothes_simple.search(line[1:]):
+        # double check that a change clothes command has not been missed
+        # by using a simpler regex which should match on more or less the same thing
         print("ERROR: irregular line", line)
         exit(-1)
 
-
+    # check for a clothes reset command
     for reg in reset_regexes:
         resetMatch = reg.search(line[1:])
         if resetMatch:
-            print(line)
+            # print(line)
+            sprite_clothes_lookup.reset_sprite_types()
 
-    # if line[0] == '+' or line[0] == '-':
-    #     if 'sprite' not in line and 'ld ' not in line:
-    #         print(line)
+    # '+' -> check for a old character sprite alias
+    # '-' -> check for ps3 sprite path
+    # ' ' -> start a new chunk of '+' and '-' type lines
+    if line[0] == '+':
+        # print(line)
+        characterMatch = character_alias.search(line)
+        if characterMatch:
+            old_character_name = characterMatch.group(1)
+            old_character_emotion = characterMatch.group(2)
+            variant = sprite_clothes_lookup.get_sprite_type(old_character_name)
+            old_sprite_filepath = get_old_sprite_path(old_character_name, old_character_emotion, variant)
+            chunks[-1].append(old_sprite_filepath)
+            # if type != 1:
+            #     print(f"Got old character alias:{old_character_name}_{old_character_emotion} type {type}")
+        else:
+            pass
+            # print("failed on", line)
+    elif line[0] == '-':
+        ps3_match = PS3SpriteLineToSpritePathRegex.search(line)
+        if ps3_match:
+            ps3_sprite_path = ps3_match.group(1)
+            chunks[-1].append(ps3_sprite_path)
+        else:
+            pass
+            # print("failed on", line)
+    elif line[0] == ' ':
+        if chunks[-1]:
+            chunks.append([])
+    else:
+        raise Exception("unknown line type", line)
 
+# if the last element of the array is an empty list, remove it
+if not chunks[-1]:
+    chunks.pop()
 
-import re
-
-#
-# class SpriteChunk:
-#     PS3SpriteLineToSpritePathRegex = re.compile(r'(sprites\\[^"]+)', re.IGNORECASE)
-#
-#     def __init__(self, old_lines, new_lines, oldSpriteAliasDatabase):
-#         """
-#         Takes a list of old and new lines, without any '+' or '-'
-#         :param old_lines:
-#         :param new_lines:
-#         """
-#
-#     def _get_old_sprite_name(self):
-#
-#     def _get_new_sprite_name(self):
-
-# generate the mapping
-
-
-
-
-
-# at this stage have something like this in each section
-#['-_ld l,":b;sprites\\cla\\1\\cla_a11_akuwarai2.png",0\n', '-_ld r,":b;sprites\\nat\\1\\nat_a11_majime1.png",24\n', '-_ld l,":b;sprites\\cla\\1\\cla_a11_def1.png",80\n', '+ld l,KLA_akuwaraiA2,0\n', '+ld r,NAT_majimeA1,24\n', '+ld l,KLA_DefA1,80\n']
-
-# print(f"Got {len(section_list)} items")
-#
-# sprite_chunks = []
-#
-# for section in section_list:
-#     print('\n\nBEGIN SECTION')
-#     old_sprites = []
-#     new_sprites = []
-#
-#     for line in section:
-#         if line[0] == '+':
-#             old_sprites.append(line[1:])
-#         elif line[0] == '-':
-#             new_sprites.append(line[1:])
-#         else:
-#             raise Exception("invalid line start character")
-#
-#     # sprite_chunks.append(SpriteChunk(old_sprites, new_sprites))
-#     sprite_chunks.append((old_sprites, new_sprites))
-#     # print(old_sprites)
-#     # print(new_sprites)
-#
-# #need to filter out bad lines from all_old like '\n' by itself!
-# all_new = []
-# for chunk in sprite_chunks:
-#     all_new.extend(chunk[1])
-#
-#
-# current_searched_line_index = 20
-# for line in currentScriptAllLines:
-#     if line == all_new[current_searched_line_index]:
-#         current_searched_line_index += 1
-#
-# print(f"completed {current_searched_line_index} out of {len(all_new)}")
-# print(f"Failed on {all_new[current_searched_line_index]}")
+for g in chunks:
+    print(g)
