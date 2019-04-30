3b524b547bcebb1641d14e0361addb77f89bc998
Daniel Wong <drojfjord@gmail.com>
2019-04-30 21:56:08 +1000

    Add background mapping

--- sprite_mapping/bg_mapping/mg_to_ps3_background_convert.py
+++ sprite_mapping/bg_mapping/mg_to_ps3_background_convert.py
@@ -1,8 +1,13 @@
+import difflib
 import glob
 import os
 import re
+from collections import defaultdict
+
+from lib import SpriteCounter
 from util import get_with_file_extension
 from typing import Tuple, List
+import operator
 
 tuple_regex = re.compile(r"""\('([^']*)', (\d+)""")
 
@@ -13,6 +18,14 @@ umineko_answer_script_input = r'C:\drojf\large_projects\umineko\umineko-answer\0
 only_used_images_folder = r'C:\temp\only_used_bg\bg'
 
 map_by_filename_path  = r'..\map_by_filename\map_by_filename.txt'
+
+# load in the mapping by filename
+map_by_filename_mapping = {}
+with open(map_by_filename_path, 'r') as inputFile:
+    for line in inputFile:
+        ps3_name, mg_path, score = line.strip().split('|')
+        print(ps3_name)
+        map_by_filename_mapping[ps3_name] = (mg_path, float(score))
 
 def deserialize_tuple_list(s : str) -> List[Tuple[str, int]]:
     stripped = s.strip().strip('[]')
@@ -34,6 +47,63 @@ def get_aliases():
 
     return aliases
 
+def get_best_ps3_matches(mangagamer_path_to_ps3_path : List[Tuple[str, List[Tuple[str, int]]]]) -> List[Tuple[str, str]]:
+    # get a list of all ps3 paths:
+    all_ps3_paths = set()
+    for mangagamer_path, ps3_path_score_tuples in mangagamer_path_to_ps3_path:
+        for ps3_path, ps3_score in ps3_path_score_tuples:
+            all_ps3_paths.add(ps3_path)
+
+    # cha_p1by
+    # for each ps3 path, accumulate which mg images match (with their scores)
+    sc = SpriteCounter()
+    for mangagamer_path, ps3_path_score_tuples in mangagamer_path_to_ps3_path:
+        for ps3_path, ps3_score in ps3_path_score_tuples:
+            sc.add_score(ps3_path, mangagamer_path, ps3_score)
+            # all_ps3_paths_mapping[ps3_path][mangagamer_path]
+
+
+    mapping = []
+    for ps3_path in all_ps3_paths:
+        mangagamer_paths_sorted = sc.get_value(ps3_path).most_common(1)
+        score = mangagamer_paths_sorted[0][1]
+        match_by_score = mangagamer_paths_sorted[0][0]
+
+        final_match = match_by_score
+
+        ps3_name, _ = os.path.splitext(ps3_path)
+        maybe_mapping = map_by_filename_mapping.get(ps3_name.lower(), None)
+        if maybe_mapping:
+            match_by_filename, score_by_filename = maybe_mapping
+            match_by_filename = os.path.join(r'BMP\background', match_by_filename)
+            if score <= 2:
+                if score_by_filename > .9:
+                    print(f"Using filename mapping as too few statistical mappings: {ps3_name} was {match_by_score} now {match_by_filename}")
+                    final_match = match_by_filename
+        else:
+            print("Maybe invalid ps3 path:", ps3_path, ps3_name)
+
+        mapping.append((ps3_path, final_match))
+
+    return mapping
+    # construct the reverse mapping (mg copy to ps3) as expected by the next stage.
+    # reverse_mapping = defaultdict(default_factory=list)
+    # for ps3_path in all_ps3_paths:
+    #     reverse_mapping.append((sc.get_value(ps3_path).most_common(1)[0][0],
+    #                             [ps3_path]))
+    #
+    #
+    # return reverse_mapping
+    # for x in reverse_mapping:
+    #     print(x)
+    # retVal = []
+    # for mangagamer_path, ps3_path_score_tuples in mangagamer_path_to_ps3_path:
+    #     best_ps3_path, best_ps3_score = max(ps3_path_score_tuples, key=operator.itemgetter(1))
+    #     #for legacy reasons, must return a list with one element here
+    #     retVal.append((mangagamer_path, [best_ps3_path]))
+    #
+    # return retVal
+
 mangagamer_path_to_ps3_path = []
 
 mangagamer_alias_to_path = get_aliases()
@@ -54,21 +124,22 @@ with open('bg_match_statistics.txt', 'r', encoding='utf-8') as statistics_file:
             print(f"WARNING: couldn't find alias for {mangagamer_alias} - skipping")
             continue
 
-        mangagamer_path_to_ps3_path.append((mangagamer_path, [get_with_file_extension(x[0],'.png') for x in ps3_bg_name_tuples]))
+        mangagamer_path_to_ps3_path.append((mangagamer_path, [(get_with_file_extension(x[0],'.png'), x[1]) for x in ps3_bg_name_tuples]))
+
+best_ps3_to_mg_mapping = get_best_ps3_matches(mangagamer_path_to_ps3_path)
+for x in best_ps3_to_mg_mapping:
+    print(x)
+
+########################################################################################################################
 
 # Check if any ps3 backgrounds were never replaced
 all_ps3_paths = set()
-for mangagamer_path, ps3_paths in mangagamer_path_to_ps3_path:
-    for ps3_path in ps3_paths:
-        all_ps3_paths.add(ps3_path.lower())
+for ps3_path, _ in best_ps3_to_mg_mapping:
+    all_ps3_paths.add(ps3_path)
+# for mangagamer_path, ps3_paths in best_ps3_to_mg_mapping:
+#     for ps3_path in ps3_paths:
+#         all_ps3_paths.add(ps3_path.lower())
 
-# load in the mapping by filename
-map_by_filename_mapping = {}
-with open(map_by_filename_path, 'r') as inputFile:
-    for line in inputFile:
-        ps3_name, mg_path = line.strip().split('|')
-        print(ps3_name)
-        map_by_filename_mapping[ps3_name] = mg_path
 
 alternative_mg_to_ps3_paths = []
 
@@ -80,17 +151,43 @@ for used_ps3_path in glob.glob(scanpath, recursive=True):
         used_ps3_name, _ = os.path.splitext(used_ps3_filename)
         alternate_mangagamer_mapping = map_by_filename_mapping.get(used_ps3_name.lower(), None)
         if alternate_mangagamer_mapping:
-            print(f"Will use {used_ps3_name} -> {alternate_mangagamer_mapping} alternate mapping")
-            alternative_mg_to_ps3_paths.append((os.path.join(r'bmp\background', alternate_mangagamer_mapping), [used_ps3_filename]))
+            print(f"Will use {used_ps3_name} -> {alternate_mangagamer_mapping[0]} alternate mapping")
+            alternative_mg_to_ps3_paths.append((used_ps3_filename, os.path.join(r'bmp\background', alternate_mangagamer_mapping[0])))
         else:
             print(f"No mapping for {used_ps3_name}")
 
-with open('simple_bg_mapping.txt', 'w') as simple_bg_mapping_file:
-    for mangagamer_path, ps3_paths in mangagamer_path_to_ps3_path + alternative_mg_to_ps3_paths:
-        ps3_paths_to_copy_to = [x for x in ps3_paths if x not in ['black.png', 'white.png']]
+########################################################################################################################
+# if the ps3 name exactly matches the mangagamer name, just use that file!
+for i, (ps3_path, _) in enumerate(best_ps3_to_mg_mapping):
+    ps3_name, _ = os.path.splitext(ps3_path)
+    mangagamer_name_by_filename_tuple = map_by_filename_mapping.get(ps3_name.lower(), None)
+    if mangagamer_name_by_filename_tuple:
+        mangagamer_name_by_filename, score = mangagamer_name_by_filename_tuple
+        if score >= 1.0:
+            full_mg_path = os.path.join(r'bmp\background', mangagamer_name_by_filename)
+            best_ps3_to_mg_mapping[i] = (ps3_path, full_mg_path)
+            print(f"ps3 name {ps3_name} is identical, so using {best_ps3_to_mg_mapping[i]}")
 
-        if mangagamer_path in ['black.png', 'white.png']:
+
+
+########################################################################################################################
+
+forced_overrides = {'door_00096.png' : r'bmp\background\garden\m_door4.png'}
+
+forced_ignore = ['anm_no0019b.png', 'anm_no0020a.png', 'blue.png', 'cake_a.png', 'chain2r_sp.png']
+
+with open('simple_bg_mapping.txt', 'w') as simple_bg_mapping_file:
+    for ps3_path, mangagamer_path in best_ps3_to_mg_mapping + alternative_mg_to_ps3_paths:
+        if mangagamer_path in ['black.png', 'white.png'] or ps3_path in ['black.png', 'white.png']:
             continue
 
-        simple_bg_mapping_file.write(' '.join([mangagamer_path] + ps3_paths_to_copy_to) + '\n')
+        override = forced_overrides.get(ps3_path, None)
+        if override:
+            mangagamer_path = override
+
+        if ps3_path in forced_ignore:
+            print(f"Manually ignored ps3 path {ps3_path}")
+            continue
+
+        simple_bg_mapping_file.write(f'{ps3_path}|{mangagamer_path}\n')
 
